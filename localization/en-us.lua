15439553d4083fc454591d1fce368eb173480962
Zack Kromer <zack.kromer@hotmail.com>
2025-02-02 21:36:28 -0700

    Finish off the grid Add general suport for perma-mult on playing cards Add localization file (en-us), currently only used for perma-mult

---
+++ localization/en-us.lua
@@ -0,0 +1,41 @@
+return {
+    descriptions = {
+        Back={},
+        Blind={},
+        Edition={},
+        Enhanced={},
+        Joker={},
+        Other={
+			card_extra_mult={
+                text={
+                    "{C:mult}+#1#{} extra mult",
+                },
+            },
+		},
+        Planet={},
+        Spectral={},
+        Stake={},
+        Tag={},
+        Tarot={},
+        Voucher={},
+    },
+    misc = {
+        achievement_descriptions={},
+        achievement_names={},
+        blind_states={},
+        challenge_names={},
+        collabs={},
+        dictionary={},
+        high_scores={},
+        labels={},
+        poker_hand_descriptions={},
+        poker_hands={},
+        quips={},
+        ranks={},
+        suits_plural={},
+        suits_singular={},
+        tutorial={},
+        v_dictionary={},
+        v_text={},
+    },
+}
